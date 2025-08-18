c1a0c26d1979d591630715d45822af541169c3eb
Trần Vũ Quyết <79063319+quyettranvu@users.noreply.github.com>
2025-08-18 15:24:07 +0700

    try Vertx apporach in reactive context

---
+++ order-reactive/src/main/java/org/acme/vertx/MultinyVertxResource.java
@@ -0,0 +1,57 @@
+package org.acme.vertx;
+
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.file.FileSystemException;
+import io.vertx.mutiny.core.Vertx;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+
+@Path("/multiny-vertx")
+public class MultinyVertxResouce {
+    
+    @Inject
+    Vertx vertx;
+
+    @GET
+    @Path("/lorem")
+    public Uni<String> getLoremIpsum() {
+        return vertx.fileSystem().readFile("lorem.txt")
+                .onItem().transform(buffer -> buffer.toString("UTF-8"));
+    }
+
+    @GET
+    @Path("/missing")
+    public Uni<String> getMissingFile() {
+        return vertx.fileSystem().readFile("oups.txt")
+                .onItem().transform(buffer -> buffer.toString("UTF-8"));
+    }
+
+    @GET
+    @Path("/recover")
+    public Uni<String> recover() {
+        return vertx.fileSystem().readFile("oups.txt")
+                .onItem().transform(buffer -> buffer.toString("UTF-8"))
+                .onFailure().recoverWithItem("oups!");
+    }
+
+    @GET
+    @Path("/404")
+    public Uni<Response> get404() {
+        return vertx.fileSystem().readFile("oups.txt")
+                .onItem().transform(buffer -> buffer.toString("UTF-8"))
+                .onItem().transform(content -> Response.ok(content).build())
+                .onFailure().recoverWithItem(Response.status(Response.Status.NOT_FOUND).build());
+    }
+
+    @ServerExceptionMapper
+    public Response mapFileSystemException(FileSystemException e) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(e.getMessage())
+                .build();
+    }
+}
